a10a2a1683dc98a9ea46ee3e1641d61ff351c38c
Vishnu P <24x7vishnu@gmail.com>
2024-07-10 01:52:52 +0530

    First commit

---
+++ script.js
@@ -0,0 +1,48 @@
+const nameEL = document.getElementById("name");
+const emailEl = document.getElementById("email");
+const ageForm = document.getElementById("ageForm");
+const submitBtn = document.getElementById("submit");
+const nameError = document.getElementById("nameError");
+const emailError = document.getElementById("emailError");
+const resultEL = document.getElementById("result")
+const regExEmail =
+  /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+const regExName = /[a-z]{3}/;
+
+ageForm.addEventListener("submit", async (submit) => {
+    submit.preventDefault();
+    
+  nameError.textContent=""
+  emailError.textContent=""
+  resultEL.textContent=""
+
+  let valid = true
+  if (!regExName.test(nameEL.value)) {
+    nameError.textContent = "Enter a valid Name";
+    nameEL.focus();
+    valid = false
+  }
+  if (!regExEmail.test(emailEl.value)) {
+    emailError.textContent = "Enter valid email";
+    emailEl.focus();
+    valid = false
+    
+  }
+  if(!valid){
+    return  
+  }
+
+  try {
+    const response = await fetch(`https://api.agify.io?name=${nameEL.value.trim()}`);
+    if (!response.ok) {
+        throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    console.log(data)
+    
+    resultEL.textContent = `Predicted age for ${nameEL.value.trim()}: ${data.age}`;
+} catch (error) {
+    resultEL.textContent = 'Error fetching data. Please try again later.';
+}
+});
+
