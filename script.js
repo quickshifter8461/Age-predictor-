2cacdc277061e2763cd8d06c332e6e09c57b05c8
Vishnu P <24x7vishnu@gmail.com>
2024-07-15 14:40:07 +0530

    style update

--- script.js
+++ script.js
@@ -4,45 +4,47 @@ const ageForm = document.getElementById("ageForm");
 const submitBtn = document.getElementById("submit");
 const nameError = document.getElementById("nameError");
 const emailError = document.getElementById("emailError");
-const resultEL = document.getElementById("result")
+const resultEL = document.getElementById("result");
 const regExEmail =
   /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 const regExName = /[a-z]{3}/;
 
 ageForm.addEventListener("submit", async (submit) => {
-    submit.preventDefault();
-    
-  nameError.textContent=""
-  emailError.textContent=""
-  resultEL.textContent=""
+  submit.preventDefault();
 
-  let valid = true
+  nameError.textContent = "";
+  emailError.textContent = "";
+  resultEL.textContent = "";
+
+  let valid = true;
   if (!regExName.test(nameEL.value)) {
-    nameError.textContent = "Enter a valid Name";
+    nameError.textContent = "Enter valid Name";
     nameEL.focus();
-    valid = false
+    valid = false;
   }
   if (!regExEmail.test(emailEl.value)) {
-    emailError.textContent = "Enter valid email";
+    emailError.textContent = "Enter valid Email";
     emailEl.focus();
-    valid = false
-    
+    valid = false;
   }
-  if(!valid){
-    return  
+  if (!valid) {
+    return;
   }
 
   try {
-    const response = await fetch(`https://api.agify.io?name=${nameEL.value.trim()}`);
+    const response = await fetch(
+      `https://api.agify.io?name=${nameEL.value.trim()}`
+    );
     if (!response.ok) {
-        throw new Error('Network response was not ok');
+      throw new Error("Network response was not ok");
     }
     const data = await response.json();
-    console.log(data)
-    
-    resultEL.textContent = `Predicted age for ${nameEL.value.trim()}: ${data.age}`;
-} catch (error) {
-    resultEL.textContent = 'Error fetching data. Please try again later.';
-}
-});
+    console.log(data);
 
+    resultEL.textContent = `Predicted age for ${nameEL.value.trim()} is ${
+      data.age
+    } years`;
+  } catch (error) {
+    resultEL.textContent = "Error fetching data. Please try again later.";
+  }
+});
